4115dc7244f80099df2c4f8e8f86b7beace2b3e3
GustavoJST <gutojusto22@gmail.com>
2026-05-25 19:47:26 -0300

    feat: use Astro Font Provider API for font management

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import { I18N } from "./src/consts";
 import { remarkModifiedTime } from "./src/lib/remark-modifed-time.mjs";
 import sitemap from "@astrojs/sitemap";
@@ -16,6 +16,26 @@ const sitemapLocales = Object.fromEntries(
 // https://astro.build/config
 export default defineConfig({
   site: "https://gustavojusto.com",
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Geist",
+      cssVariable: "--geist-variable",
+      weights: ["100 900"],
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Geist Sans",
+      cssVariable: "--geist-sans",
+      weights: ["100 900"],
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Geist Mono",
+      cssVariable: "--geist-mono",
+      weights: ["100 900"],
+    },
+  ],
   i18n: {
     locales: Object.keys(I18N.LANGUAGES), // ["en", "pt-br", etc]
     defaultLocale: I18N.DEFAULT_LANGUAGE,
